dc49276159f806c11b7a5a90c6840858929d4dd3
BrandedContentGrupoGodo <153719709+BrandedContentGrupoGodo@users.noreply.github.com>
2024-07-26 12:02:33 +0200

    Add files via upload

---
+++ iryo-brl/assets/js/scripts.js
@@ -0,0 +1,31 @@
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+    const isMobile = window.innerWidth <= 768;
+
+    if (!isMobile) {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".wrapper",
+                start: "top top",
+                end: "+=150%",
+                pin: true,
+                scrub: true,
+                markers: false // Puedes eliminar los marcadores después de las pruebas
+            }
+        })
+        .to(".image-container img", {
+            scale: 2,
+            z: 350,
+            transformOrigin: "center center",
+            ease: "power1.inOut"
+        })
+        .to(".section.hero", {
+            scale: 1.1,
+            transformOrigin: "center center",
+            ease: "power1.inOut"
+        }, "<");
+    }
+});
