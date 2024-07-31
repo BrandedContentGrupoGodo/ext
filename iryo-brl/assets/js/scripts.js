89f2e038c3c3ad737379f481b445748d2c34c57e
BrandedContentGrupoGodo <153719709+BrandedContentGrupoGodo@users.noreply.github.com>
2024-07-31 11:00:11 +0200

    Update scripts.js

--- iryo-brl/assets/js/scripts.js
+++ iryo-brl/assets/js/scripts.js
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
             scrollTrigger: {
                 trigger: ".wrapper",
                 start: "top top",
-                end: "+=350%",
+                end: "+=150%",
                 pin: true,
                 scrub: true,
                 markers: false // Puedes eliminar los marcadores después de las pruebas
