4b0975c8cd328b9cd7ffc5cd53ce4e19817d8d66
BrandedContentGrupoGodo <153719709+BrandedContentGrupoGodo@users.noreply.github.com>
2025-09-19 12:46:04 +0200

    Update scripts.js

--- caixabank-2025/01_09-07-25/assets/js/scripts.js
+++ caixabank-2025/01_09-07-25/assets/js/scripts.js
@@ -57,7 +57,7 @@ const historias = [
     nombre: "Sergio",
     frase: "Los rivales representan lo mejor de mí",
     imagen: "https://brandedcontentgrupogodo.github.io/ext/caixabank-2025/01_09-07-25/assets/img/cap2.png",
-    enlace: "https://www.lavanguardia.com/deportes/20250909/11009017/elena-congost-atleta-perdio-medalla-guia-constelaciones-streaming-brl.html"
+    enlace: "https://www.lavanguardia.com/deportes/20250922/11076618/sergio-garrote-hand-bike-espanol-campeon-paralimpico-juega-vida-entrenamiento-leyenda-deporte-constelaciones-streaming-brl.html"
   },
   {
     nombre: "Nico",
